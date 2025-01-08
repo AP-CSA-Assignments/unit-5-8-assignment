8b0956bade72b6bd9a58cbe07a1dbd95deb34672
Kyle Vu <ktvu@hbuhsd.edu>
2025-01-07 17:49:06 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,42 +4,22 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Uncomment and complete the code below to test your Plane class
-    /* Delete this line to uncomment code **************************
     Scanner sc = new Scanner(System.in);
     String input = "";
-    Plane p = new Plane(1000);
     while (!input.equals("q"))
     {
-      System.out.println(p);
-      System.out.println("Type \"u\" to move upwards, \"d\" to move downwards, \"n\" for new Plane, \"q\" to quit.");
-      // System.out.println(print the plane's location and uncomment);
+      System.out.println("Input the make of the car, \"default\" to create a default car or \"q\" to quit:");
       input = sc.nextLine();
-
-      if (input.equals("u"))
+      if (input.equals("default"))
       {
-        // move the plane upwards
-      }
-      else if (input.equals("d"))
-      {
-        // move the plane downwards
-      }
-      else if (input.equals("n"))
-      {
-        System.out.println("Starting location for a new plane?");
-        int location = sc.nextInt();
-        sc.nextLine(); // consume the ENTER key left behind by nextInt()
-        p = // create new plane at proper location
-      }
-      else if (input.equals("q"))
-      {
-        System.out.println("Thank you for using plane simulator");
+        // create default car
       }
       else
       {
-        System.out.println("Invalid input");
+        // ask for model, year, miles per gallon
       }
+
+      System.out.println(); // print out the car object
     }
-    Delete this line to uncomment ************************************/
   }
 }
